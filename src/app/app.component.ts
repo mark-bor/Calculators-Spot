6069405b55f29c5eae26e0a7b0397c4df376dfcb
Mark Bordakov <mark12bord@gmail.com>
2024-03-11 18:22:30 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type SectionData = {
+  section: string,
+  links: string[]
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +14,37 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  sections: SectionData[] = [
+    {
+      section: 'Financial',
+      links: [
+        'exchange-rate',
+        'percent',
+        'investments',
+      ]
+    },
+    {
+      section: 'Math',
+      links: [
+        'quadratic-equation', 
+        'pythagorean-theorem',
+        'circle',
+      ]
+    },
+  ];
+  
+  isMenu: boolean = false;
+
+  upperFirstLetter(word: string) {
+    const changedWord = word.split('-').join(' ');
+    return changedWord.charAt(0).toUpperCase() + changedWord.slice(1)
+  }
+
+  toggleMenu() {
+    this.isMenu = !this.isMenu;
+  }
+
+  setMenu(param: boolean) {
+    this.isMenu = param;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
+import { PercentComponent } from './percent/percent.component';
+import { InvestmentsComponent } from './investments/investments.component';
+
+import { QuadraticEquationComponent } from './quadratic-equation/quadratic-equation.component';
+import { PythagoreanTheoremComponent } from './pythagorean-theorem/pythagorean-theorem.component';
+import { CircleComponent } from './circle/circle.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent, title: 'Home page'},
+
+    {path: 'exchange-rate', component: ExchangeRateComponent, title: 'Exchange rate page'},
+    {path: 'percent', component: PercentComponent, title: 'Investment page'},
+    {path: 'investments', component: InvestmentsComponent, title: 'Investments page'},
+
+    {path: 'quadratic-equation', component: QuadraticEquationComponent, title: 'Quadratic equation page'},
+    {path: 'pythagorean-theorem', component: PythagoreanTheoremComponent, title: 'Pythagorean theorem page'},
+    {path: 'circle', component: CircleComponent, title: 'Circle page'},
+];
